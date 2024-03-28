32b26061ceffd2ebb8f5d451261f7a2f82d34cb6
Dharmi Javiya <dharmijaviya@gmail.com>
2024-03-28 13:00:54 +0530

    second commit

--- src/main/java/org/example/springtest/SpringtestApplication.java
+++ src/main/java/org/example/springtest/SpringtestApplication.java
@@ -2,12 +2,19 @@ package org.example.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringtestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringtestApplication.class, args);
+    }
+
+    @GetMapping("/greetings")
+
+    public String greetings() {
+        return "Hello World!";
     }
 
 }
